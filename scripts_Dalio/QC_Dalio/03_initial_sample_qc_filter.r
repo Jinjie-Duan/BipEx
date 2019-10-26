a0c6b45c426669efda8591ee4dbfe05f805a8fa4
astheeggeggs <duncan.stuart.palmer@gmail.com>
2019-10-26 18:50:31 -0400

    added to and updated website

--- scripts_Dalio/QC_Dalio/03_initial_sample_qc_filter.r
+++ scripts_Dalio/QC_Dalio/03_initial_sample_qc_filter.r
@@ -10,35 +10,46 @@ low_coverage <- fread("Dalio_Low_Coverage_Samples.txt")$SAMPLE
 
 QC_FILE <- "gsutil cat gs://dalio_bipolar_w1_w2_hail_02/data/samples/03_initial_sample_qc.tsv"
 df <- fread(QC_FILE, sep='\t', header=TRUE, data.table=FALSE)
-names(df) <- gsub("phenotype\\.", "", names(df))
 names(df) <- gsub("qc\\.", "", names(df))
 
 # Also want to count the number of samples not present in the vcf file.
-df_pheno <- fread("../../phenotype_data/BIP_phenotype_information_cleaned_new_subtype_information_added.tsv")
-df_pheno <- df_pheno[-which(is.na(df_pheno$PI)),]
+# Note that throughout here, we are merging in BIP_phenotype_information_cleaned_new_subtype_information_added_and_harmonised_final.tsv
+# rather than relying on the annotated phenotype information, due to changes that occurred.
+df_pheno <- fread("../../phenotype_data/BIP_phenotype_information_cleaned_new_subtype_information_added_and_harmonised_final.tsv") %>% rename(s=SAMPLE_ALIAS)
+# df_pheno <- df_pheno[-which(is.na(df_pheno$PI)),]
 
 # Remove samples with unknown phenotype, those from 1kg that were included, and those with high
 # contamination or coverage as defined by the spreadsheet from Laura.
-df <- df[-which(is.na(df$PI)),]
+# df <- df[-which(is.na(df$PI)),]
+
+df_merged <- merge(df_pheno, df,  by='s')
 
 df_initial_summary_count <- data.table(
-	Filter = c("Initial samples", "Unknown phenotype", "Low coverage or high contamination"),
+	Filter = c(
+		"Initial samples in vcf", 
+		"Unable to phenotype and sequence information",
+		"Unknown phenotype",
+		"Low coverage or high contamination"),
 	Samples = c(nrow(df),
-				nrow(filter(df, PHENOTYPE_COARSE=='Unknown')),
-				nrow(df[which(df$s %in% low_coverage),])),
-	"Bipolar cases" = c(nrow(df %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				nrow(filter(df, PHENOTYPE_COARSE == "Bipolar Disorder") %>% filter(PHENOTYPE_COARSE == 'Unknown')),
-				nrow(df[which((df$s %in% low_coverage) & df$PHENOTYPE_COARSE == "Bipolar Disorder"),])),
-	"Controls" = c(nrow(df %>% filter(PHENOTYPE_COARSE == "Control")),
-				nrow(filter(df, PHENOTYPE_COARSE == "Control") %>% filter(PHENOTYPE_COARSE == 'Unknown')),
-				nrow(df[which((df$s %in% low_coverage) & df$PHENOTYPE_COARSE == "Control"),])))
+				nrow(df) - nrow(df_merged),
+				nrow(filter(df_merged, PHENOTYPE_COARSE_NEW=='Unknown')),
+				nrow(df_merged[which(df_merged$s %in% low_coverage),])),
+	"Bipolar cases" = c(nrow(df_merged %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				NA,
+				NA,
+				nrow(df_merged[which((df_merged$s %in% low_coverage) & df_merged$PHENOTYPE_COARSE_NEW == "Bipolar Disorder"),])),
+	"Controls" = c(nrow(df_merged %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				NA,
+				NA,
+				nrow(df_merged[which((df_merged$s %in% low_coverage) & df_merged$PHENOTYPE_COARSE_NEW == "Control"),])))
 
-df <- df[-which(df$s %in% low_coverage),] %>% filter(PHENOTYPE_COARSE != 'Unknown')
-df_initial_summary_count <- rbindlist(list(df_initial_summary_count, list("Samples after initial filter", nrow(df),
-	nrow(df %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-	nrow(df %>% filter(PHENOTYPE_COARSE == "Control")))), use.names=FALSE)
+df_merged <- df_merged[-which(df_merged$s %in% low_coverage),] %>% filter(PHENOTYPE_COARSE_NEW != 'Unknown')
+df_initial_summary_count <- rbindlist(list(df_initial_summary_count, list("Samples after initial filter", nrow(df_merged),
+	nrow(df_merged %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+	nrow(df_merged %>% filter(PHENOTYPE_COARSE_NEW == "Control")))), use.names=FALSE)
 fwrite(df_initial_summary_count, file='../../samples_Dalio/03_initial_sample_count.tsv', quote=FALSE, row.names=FALSE, col.names=FALSE, sep='\t')
 
+df <- df_merged
 names(df) <- gsub("qc_padded_ice\\.", "", names(df))
 
 df_out <- filter(df, call_rate > T_sample_callRate) %>%
@@ -54,7 +65,7 @@ fwrite(df_out, file=SAMPLE_LIST_INITIAL_QC, quote=FALSE, row.names=FALSE, col.na
 
 # Create the table too
 df_summary_count <- data.table(
-	Filter = c("Initial samples",
+	Filter = c("Samples after initial filter",
 			   paste0("Sample call rate < ", T_sample_callRate),
 			   paste0("% FREEMIX contamination > ", T_pct_contamination),
 			   paste0("% chimeric reads > ", T_pct_chimeras),
@@ -68,19 +79,19 @@ df_summary_count <- data.table(
 				nrow(filter(df, dp_stats.mean <= T_dpMean)),
 				nrow(filter(df, gq_stats.mean <= T_gqMean)),
 				nrow(df_out)),
-	"Bipolar Cases" = c(nrow(df %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-			    nrow(filter(df, call_rate <= T_sample_callRate) %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				nrow(filter(df, PCT_CONTAMINATION >= T_pct_contamination) %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				nrow(filter(df, PCT_CHIMERAS >= T_pct_chimeras) %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				nrow(filter(df, dp_stats.mean <= T_dpMean) %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				nrow(filter(df, gq_stats.mean <= T_gqMean) %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-				length(which(df_out$s %in% (df %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder"))$s))),
-	Controls = c(nrow(df %>% filter(PHENOTYPE_COARSE == "Bipolar Disorder")),
-			    nrow(filter(df, call_rate <= T_sample_callRate) %>% filter(PHENOTYPE_COARSE == "Control")),
-				nrow(filter(df, PCT_CONTAMINATION >= T_pct_contamination) %>% filter(PHENOTYPE_COARSE == "Control")),
-				nrow(filter(df, PCT_CHIMERAS >= T_pct_chimeras) %>% filter(PHENOTYPE_COARSE == "Control")),
-				nrow(filter(df, dp_stats.mean <= T_dpMean) %>% filter(PHENOTYPE_COARSE == "Control")),
-				nrow(filter(df, gq_stats.mean <= T_gqMean) %>% filter(PHENOTYPE_COARSE == "Control")),
-				length(which(df_out$s %in% (df %>% filter(PHENOTYPE_COARSE == "Control"))$s))))
+	"Bipolar Cases" = c(nrow(df %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+			    nrow(filter(df, call_rate <= T_sample_callRate) %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				nrow(filter(df, PCT_CONTAMINATION >= T_pct_contamination) %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				nrow(filter(df, PCT_CHIMERAS >= T_pct_chimeras) %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				nrow(filter(df, dp_stats.mean <= T_dpMean) %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				nrow(filter(df, gq_stats.mean <= T_gqMean) %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+				length(which(df_out$s %in% (df %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder"))$s))),
+	Controls = c(nrow(df %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+			    nrow(filter(df, call_rate <= T_sample_callRate) %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				nrow(filter(df, PCT_CONTAMINATION >= T_pct_contamination) %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				nrow(filter(df, PCT_CHIMERAS >= T_pct_chimeras) %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				nrow(filter(df, dp_stats.mean <= T_dpMean) %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				nrow(filter(df, gq_stats.mean <= T_gqMean) %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+				length(which(df_out$s %in% (df %>% filter(PHENOTYPE_COARSE_NEW == "Control"))$s))))
 
 fwrite(df_summary_count, file='../../samples_Dalio/03_sample_count.tsv', quote=FALSE, row.names=FALSE, col.names=FALSE, sep='\t')
--- scripts_Dalio/QC_Dalio/15_filter_final_sample_qc.r
+++ scripts_Dalio/QC_Dalio/15_filter_final_sample_qc.r
@@ -8,42 +8,50 @@ source("r_functions_and_parameters/r_options_Dalio.r")
 df_after <- fread(SAMPLE_AFTER_QC_FILE, sep='\t', stringsAsFactors=FALSE, header=TRUE, data.table=FALSE) %>%
   mutate(phase='After Variant QC')
 
+# Check that the phenotypes match, if they don't we should use this dataframe.
+dt_pheno <- fread("../../phenotype_data/BIP_phenotype_information_cleaned_new_subtype_information_added_and_harmonised.tsv") %>% rename(s=SAMPLE_ALIAS) %>% select(s, PHENOTYPE_COARSE_NEW)
+if(any(duplicated(dt_pheno$s))) {
+  dt_pheno <- dt_pheno[-which(duplicated(dt_pheno$s)),]
+}
+
+df_after_merged <- merge(df_after, dt_pheno, by='s')
+
 df_keep <- df_after['s']
 print(paste0("Started with: ", nrow(df_keep), " samples"))
 
 n_SDs <- 3
 
 # r_ti_tv
-df_keep_ti_tv <- group_by(df_after, phenotype.PROJECT_OR_COHORT) %>%
+df_keep_ti_tv <- group_by(df_after_merged, phenotype.PROJECT_OR_COHORT) %>%
   summarise(mean=mean(sample_qc.r_ti_tv), sd=sd(sample_qc.r_ti_tv)) %>%
-  inner_join(df_after, by='phenotype.PROJECT_OR_COHORT') %>%
+  inner_join(df_after_merged, by='phenotype.PROJECT_OR_COHORT') %>%
   filter((sample_qc.r_ti_tv >= mean - n_SDs*sd & sample_qc.r_ti_tv <= mean + n_SDs*sd) | is.na(sd))
 
 df_keep <- df_keep_ti_tv %>% inner_join(df_keep, by='s')
 print(paste0("Remove Ti/Tv outliers: ", nrow(df_keep), " samples remain"))
 
 # r_het_hom_var
-df_keep_het_hom_var <- group_by(df_after, phenotype.PROJECT_OR_COHORT) %>%
+df_keep_het_hom_var <- group_by(df_after_merged, phenotype.PROJECT_OR_COHORT) %>%
   summarise(mean=mean(sample_qc.r_het_hom_var), sd=sd(sample_qc.r_het_hom_var)) %>%
-  inner_join(df_after, by='phenotype.PROJECT_OR_COHORT') %>%
+  inner_join(df_after_merged, by='phenotype.PROJECT_OR_COHORT') %>%
   filter((sample_qc.r_het_hom_var >= mean - n_SDs*sd & sample_qc.r_het_hom_var <= mean + n_SDs*sd) | is.na(sd))
 
 df_keep <- df_keep_het_hom_var %>% inner_join(df_keep, by='s')
 print(paste0("Remove Het/HomVar outliers: ", nrow(df_keep), " samples remain"))
 
 # r_insertion_deletion
-df_keep_insertion_deletion <- group_by(df_after, phenotype.PROJECT_OR_COHORT) %>%
+df_keep_insertion_deletion <- group_by(df_after_merged, phenotype.PROJECT_OR_COHORT) %>%
   summarise(mean=mean(sample_qc.r_insertion_deletion), sd=sd(sample_qc.r_insertion_deletion)) %>%
-  inner_join(df_after, by='phenotype.PROJECT_OR_COHORT') %>%
+  inner_join(df_after_merged, by='phenotype.PROJECT_OR_COHORT') %>%
   filter((sample_qc.r_insertion_deletion >= mean - n_SDs*sd & sample_qc.r_insertion_deletion <= mean + n_SDs*sd) | is.na(sd))
 
 df_keep <- df_keep_insertion_deletion %>% inner_join(df_keep, by='s')
 print(paste0("Remove Ins/Del outliers: ", nrow(df_keep), " samples remain"))
 
 # n_singletons
-df_keep_n_singletons <- group_by(df_after, phenotype.LOCATION) %>%
+df_keep_n_singletons <- group_by(df_after_merged, phenotype.LOCATION) %>%
   summarise(mean=mean(sample_qc.n_singleton), sd=sd(sample_qc.n_singleton)) %>%
-  inner_join(df_after, by='phenotype.LOCATION') %>%
+  inner_join(df_after_merged, by='phenotype.LOCATION') %>%
   filter((sample_qc.n_singleton >= mean - n_SDs*sd & sample_qc.n_singleton <= mean + n_SDs*sd) | is.na(sd))
 
 df_keep <- df_keep_n_singletons %>% inner_join(df_keep, by='s')
@@ -55,25 +63,25 @@ df_final_sample_summary <- data.table(Filter = c("Samples after population filte
                           paste0("Within batch Het/HomVar ratio outside ", n_SDs, " standard deviations"),
                           paste0("Within batch Insertion/Deletion ratio outside ", n_SDs, " standard deviations"),
                           paste0("Within location n singletons outside ", n_SDs, " standard deviations"),
-                          "Samples after filters"),
-                     Samples = c(nrow(df_after),
-                                nrow(df_after) - nrow(df_keep_ti_tv),
-                                nrow(df_after) - nrow(df_keep_het_hom_var),
-                                nrow(df_after) - nrow(df_keep_insertion_deletion),
-                                nrow(df_after) - nrow(df_keep_n_singletons),
+                          "Samples after final sample filters"),
+                     Samples = c(nrow(df_after_merged),
+                                nrow(df_after_merged) - nrow(df_keep_ti_tv),
+                                nrow(df_after_merged) - nrow(df_keep_het_hom_var),
+                                nrow(df_after_merged) - nrow(df_keep_insertion_deletion),
+                                nrow(df_after_merged) - nrow(df_keep_n_singletons),
                                 nrow(df_keep)),
-                     'Bipolar Cases' = c(nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Bipolar Disorder")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Bipolar Disorder")) - nrow(df_keep_ti_tv %>% filter(phenotype.PHENOTYPE_COARSE == "Bipolar Disorder")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Bipolar Disorder")) - nrow(df_keep_het_hom_var %>% filter(phenotype.PHENOTYPE_COARSE == "Bipolar Disorder")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Bipolar Disorder")) - nrow(df_keep_insertion_deletion %>% filter(phenotype.PHENOTYPE_COARSE == "Bipolar Disorder")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Bipolar Disorder")) - nrow(df_keep_n_singletons %>% filter(phenotype.PHENOTYPE_COARSE == "Bipolar Disorder")),
-                      nrow(df_keep %>% filter(phenotype.PHENOTYPE_COARSE.x=="Bipolar Disorder"))),
-                     Controls = c(nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Control")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Control")) - nrow(df_keep_ti_tv %>% filter(phenotype.PHENOTYPE_COARSE == "Control")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Control")) - nrow(df_keep_het_hom_var %>% filter(phenotype.PHENOTYPE_COARSE == "Control")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Control")) - nrow(df_keep_insertion_deletion %>% filter(phenotype.PHENOTYPE_COARSE == "Control")),
-                      nrow(df_after %>% filter(phenotype.PHENOTYPE_COARSE=="Control")) - nrow(df_keep_n_singletons %>% filter(phenotype.PHENOTYPE_COARSE == "Control")),
-                      nrow(df_keep %>% filter(phenotype.PHENOTYPE_COARSE.x=="Control"))))
+                     'Bipolar Cases' = c(nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Bipolar Disorder")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Bipolar Disorder")) - nrow(df_keep_ti_tv %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Bipolar Disorder")) - nrow(df_keep_het_hom_var %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Bipolar Disorder")) - nrow(df_keep_insertion_deletion %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Bipolar Disorder")) - nrow(df_keep_n_singletons %>% filter(PHENOTYPE_COARSE_NEW == "Bipolar Disorder")),
+                      nrow(df_keep %>% filter(PHENOTYPE_COARSE_NEW.x=="Bipolar Disorder"))),
+                     Controls = c(nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Control")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Control")) - nrow(df_keep_ti_tv %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Control")) - nrow(df_keep_het_hom_var %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Control")) - nrow(df_keep_insertion_deletion %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+                      nrow(df_after_merged %>% filter(PHENOTYPE_COARSE_NEW=="Control")) - nrow(df_keep_n_singletons %>% filter(PHENOTYPE_COARSE_NEW == "Control")),
+                      nrow(df_keep %>% filter(PHENOTYPE_COARSE_NEW.x=="Control"))))
 
 fwrite(df_final_sample_summary, file='../../samples_Dalio/15_sample_count.tsv', quote=FALSE, row.names=FALSE, col.names=FALSE, sep='\t')
 
